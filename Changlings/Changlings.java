136d336135dd8593590569e95f225d8442c8ca48
Nathaniel <sauerergn@carleton.edu>
2017-03-12 18:07:15 -0500

    Break code into methods to make more readable, throw exception for # of runArgs

--- Changlings/Changlings.java
+++ Changlings/Changlings.java
@@ -6,51 +6,70 @@ import java.util.*;
 // where each step changes only 1 letter in the word
 public class Changlings {
 
+   private static Map<String, Set<String>> graph;
+   private static String startNode;
+   private static String endNode;
+   private static Map<String, String> prevNodes;
+
    public static void main (String[] args) throws FileNotFoundException{
+      if (args.length != 3)
+         throw new UnsupportedOperationException("\nThis program can only be run with 3 " +
+            "command-line args: a file location and two words.\nPlease run it again with new args");
    
-      Map<String, Set<String>> graph = new HashMap<>();
-      Scanner input = new Scanner(new File(args[0]));
-      
+      // initialize class variables
+      graph = new HashMap<>();
+      startNode = args[1];
+      endNode = args[2];
+      prevNodes = new HashMap<>();      
+    
       // for each word in the input file
       // checks all words in the file to see if they are connections
       // and puts a set of all connections into the map
+      Scanner input = new Scanner(new File(args[0]));      
       while(input.hasNextLine()){
-         String key = input.nextLine(); 
-         Set<String> connections = new HashSet<>();
-         
-         // check every string in the original input for connection
-         Scanner tempInput = new Scanner(new File(args[0]));         
-         while (tempInput.hasNextLine()){
-            String nextWord = tempInput.nextLine();
-            if ((nextWord.length() == key.length()) && (!nextWord.equals(key)))
-               // for each position in key, if the rest of the strings are the same
-               for (int i = 0; i < key.length(); i++){
-                  if ((key.substring(0,i) + key.substring(i+1, key.length())).equals(
-                  (nextWord.substring(0,i) + nextWord.substring(i+1, key.length()))))
-                     connections.add(nextWord);
-               }
-         }
-         tempInput.close();
-         graph.put(key, connections);
+         String key = input.nextLine();
+         putConnections(key, args[0]); 
       }
       input.close();
-   
-      String startNode = args[1];
-      String endNode = args[2];
       
-      // Do a Breadth-First Search from startWord for endWord
-      // Set up queue for BFS, create a map of all keys/nodes
-      // init all keys' values to null but start to "start"            
-      Queue<String> queue = new ArrayDeque<String>();
-      Map<String, String> prevNodes = new HashMap<>();
+      // init all keys' values in prevNodes to "notVisited" except startNode   
       Set<String> allKeys = graph.keySet();
       for (String key : allKeys){
-         prevNodes.put(key, "null");
+         prevNodes.put(key, "notVisited");
       }
       prevNodes.replace(startNode, "starting node");
-      boolean found = false;
-      
+   
+      BFS();      
+      printSolution(); 
+   }
+   
+   // finds all connections for @param key and puts them into the graph
+   private static void putConnections(String key, String fileLocation) throws FileNotFoundException{
+      // check every string in the original input for connection
+      Scanner tempInput = new Scanner(new File(fileLocation));
+      Set<String> connections = new HashSet<>();         
+      while (tempInput.hasNextLine()){
+         String nextWord = tempInput.nextLine();
+         if ((nextWord.length() == key.length()) && (!nextWord.equals(key)))
+            // for each position in key, if the rest of the strings are the same
+            for (int i = 0; i < key.length(); i++){
+               if ((key.substring(0,i) + key.substring(i+1, key.length())).equals(
+                  (nextWord.substring(0,i) + nextWord.substring(i+1, key.length()))))
+                  connections.add(nextWord);
+            }
+      }
+      tempInput.close();
+      graph.put(key, connections);
+   }
+   
+   // Do a Breadth-First Search from startWord for endWord
+   // Set up queue for BFS, create a map of all keys/nodes 
+   private static void BFS(){   
+      Queue<String> queue = new ArrayDeque<String>();
+      boolean found = false;      
       queue.offer(startNode);
+      // adds all unvisited connections of current Node to the queue 
+      // to continue BFS; until end found or the queue runs out
       while (!found && !queue.isEmpty()){
          String currentNode = queue.poll();
          Set<String> connections = graph.get(currentNode);
@@ -60,17 +79,21 @@ public class Changlings {
                prevNodes.replace(nextNode, currentNode);
                found = true;
                break; 
-            } 
-            // else if nextNode not visited, visit and offer
-            else if (prevNodes.replace(nextNode, "null", currentNode)){
+            }// else if nextNode not visited, visit and offer
+            else if (prevNodes.replace(nextNode, "notVisited", currentNode)){
                queue.offer(nextNode);
             }
          }
       }
-      // doesn't print original
-      if (!prevNodes.get(endNode).equals("null")){
-         String currentNode = endNode;
+   }
+   
+   // Prints solution if the endNode was visited (solution was found) 
+   private static void printSolution(){
+      if (!prevNodes.get(endNode).equals("notVisited")){
+         // pushes keys onto stack from end to start
+         // so it can print them out from first to last
          Stack<String> stack = new Stack<String>();
+         String currentNode = endNode;
          while (!prevNodes.get(currentNode).equals("starting node")){
             stack.push(currentNode);
             currentNode = prevNodes.get(currentNode);
@@ -79,8 +102,7 @@ public class Changlings {
          while (!stack.empty()) 
             System.out.println(stack.pop());                  
       } 
-      else {
+      else 
          System.out.println("No solution found.");
-      }
    }
 }
